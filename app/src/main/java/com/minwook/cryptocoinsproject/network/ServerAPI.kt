8d670ca323373dc9125adf8936d0c277c0033064
minwook <pmpmopm1@naver.com>
2021-06-16 07:15:23 +0900

    삭제된 코드 commit

--- app/src/main/java/com/minwook/cryptocoinsproject/network/ServerAPI.kt
+++ app/src/main/java/com/minwook/cryptocoinsproject/network/ServerAPI.kt
@@ -1,11 +1,6 @@
 package com.minwook.cryptocoinsproject.network
 
-import com.minwook.cryptocoinsproject.data.ExchangeInfo
 import com.minwook.cryptocoinsproject.data.Ticker
-import com.tinder.scarlet.WebSocket
-import com.tinder.scarlet.ws.Receive
-import com.tinder.scarlet.ws.Send
-import io.reactivex.Flowable
 import io.reactivex.Single
 import retrofit2.http.GET
 
