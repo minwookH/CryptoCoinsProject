2c94815f379060cc4351f1ac2d30846efa9c3ace
minwook <pmpmopm1@cashwalk.io>
2021-06-16 18:28:01 +0900

    상세 페이지 코인 이미지 추가

--- app/src/main/java/com/minwook/cryptocoinsproject/BindingAdapter.kt
+++ app/src/main/java/com/minwook/cryptocoinsproject/BindingAdapter.kt
@@ -1,19 +1,39 @@
 package com.minwook.cryptocoinsproject
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
 object BindingAdapter {
 
     @JvmStatic
     @BindingAdapter("textColor")
-    fun bindGoodImage(view: TextView, percent: String) {
+    fun bindCoinTextColor(view: TextView, percent: String) {
         when {
-            percent.toDouble() > 0 -> view.setTextColor(ContextCompat.getColor(view.context, R.color.red))
-            percent.toDouble() == 0.0 -> view.setTextColor(ContextCompat.getColor(view.context, R.color.black))
+            percent.toDouble() > 0 -> view.setTextColor(
+                ContextCompat.getColor(
+                    view.context,
+                    R.color.red
+                )
+            )
+            percent.toDouble() == 0.0 -> view.setTextColor(
+                ContextCompat.getColor(
+                    view.context,
+                    R.color.black
+                )
+            )
             else -> view.setTextColor(ContextCompat.getColor(view.context, R.color.blue))
         }
+    }
+
+    @JvmStatic
+    @BindingAdapter("coinImage")
+    fun bindCoinImage(view: ImageView, symbol: String) {
+        Glide.with(view.context)
+            .load("https://cryptoicons.org/api/icon/${symbol}/200".toLowerCase())
+            .into(view)
     }
 
 }
